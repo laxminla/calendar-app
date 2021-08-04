1a091bf55d7ad59945d8eebe25bec73ffed1390c
Laxmi Sharma <laxminla@yahoo.com>
2021-08-03 17:26:13 -0700

    second commit

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,17 +1,52 @@
+
+
 var timeDisplayEl = $('#currentDay');
 const container = document.querySelector('.container')
 const clicked = document.querySelector('.clicked')
 const tasks = []
+var timeList = ['9', '10', '11', '12', '13', '14', '15', '16', '17']
 
 function displayTime() {
     var today = moment().format('MMMM Do YYYY, h:mm:ss a');
     timeDisplayEl.text(today)
 }
+
+
+function calendar() {
+    var now = new Date().getHours();
+    var check;
+
+    if (now > 12) {
+        check = now % 12 + "PM"
+    }
+    else {
+        check = now + "AM"
+    }
+
+    for (let i = 0; i < timeList.length; ++i) {
+        if (timeList[i] < now) {
+            var elem = document.getElementsByName(timeList[i]);
+            elem[0].id = "past";
+        }
+        else if (timeList[i] > now) {
+            var elem = document.getElementsByName(timeList[i]);
+            elem[0].id = "future";
+        }
+        else {
+            var elem = document.getElementsByName(timeList[i]);
+            elem[0].id = "present";
+        }
+
+    }
+}
+
+
+function saveEvent() {
+
+}
+
 setInterval(displayTime, 1000);
-
-
-
-
+calendar();
 
 // for(let i = 0; i < 10; i++){
 //   const holder = document.createElement('div')
@@ -27,7 +62,7 @@ setInterval(displayTime, 1000);
 //   clicked.textContent = e.target.previousSibling.value
 //   task.push(e.target.previousSibling.value)
 //   window.localStorage.setItem(
-//     'task', 
+//     'task',
 //     JSON.stringify(tasks)
 //   )
 // })
